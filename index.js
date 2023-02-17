b4675d4f9f7cfba4e2ef82462e7052008cbec8fd
Atharva Joshi <69634375+atharva20-coder@users.noreply.github.com>
2023-02-18 03:48:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const laplaceTransform = require("./functions/laplace.js");
 
+const solveKMap = require("./functions/karnaughMap");
+
+const solveKMap = require("./functions/causalNon");
+
 module.exports = laplaceTransform;
+module.exports = solveKMap;
+module.exports = isCausalSystem;
